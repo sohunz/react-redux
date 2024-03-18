b5547f239fd41ec9813d02b8df7d09b03d89a2aa
sohunz <sohunily@gmail.com>
2024-03-18 09:17:58 +0700

    setup redux-toolkit with react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./components/Home";
+
+const App = () => {
+    return (
+        <div className="m-10">
+            <Home />
+        </div>
+    );
+};
+
+export default App;
